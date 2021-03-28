eb3bea054c2be9d5b510bb67d6912b2e79f0b6a9
Wan Zulmuhammad Harith <wzharith@gmail.com>
2021-03-29 01:53:17 +0800

    add models

--- apps/sofia/models.py
+++ apps/sofia/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Order(models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    paid_amount = models.FloatField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    shipped_date = models.DateTimeField(auto_now_add=True) #blank=True, null=True
+
+    def __str__(self):
+        return '%s' % self.name
