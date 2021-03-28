4d7555923b4463407706db854b6bff00433fdfa0
Wan Zulmuhammad Harith <wzharith@gmail.com>
2021-03-29 00:35:23 +0800

    add dashboard

--- apps/sofia/views.py
+++ apps/sofia/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
+from django import template
 
-# Create your views here.
+
+def index(request):
+    context = {}
+    context['segment'] = 'index'
+
+    html_template = loader.get_template('index.html')
+    return HttpResponse(html_template.render(context, request))
--- sofiadash/settings.py
+++ sofiadash/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+CORE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -56,8 +58,7 @@ ROOT_URLCONF = 'sofiadash.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,3 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(CORE_DIR, 'static'),
+)
